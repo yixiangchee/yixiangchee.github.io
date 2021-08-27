9abd7c9c5a56b34e924195352bfd495d46f92763
Yi Xiang Chee <yixiangchee@Yis-MacBook-Air.local>
2021-08-27 13:27:02 +0800

    Updates

--- src/Photography.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Photography = () => {
-    return(<div>
-        this is photography
-    </div>)
-}
-
-export default Photography;
--- src/Work.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Work = () => {
-    return(<div>
-        this is work
-    </div>)
-}
-
-export default Work;
